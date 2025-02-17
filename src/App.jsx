d2b5bba064570d1ea599b274bbbb9618eba25063
Saksham <agsaksham07@gmail.com>
2025-02-17 16:43:41 +0530

    init

---
+++ src/App.jsx
@@ -0,0 +1,18 @@
+import { useState } from 'react'
+import './App.css'
+import { Button } from './components/ui/button'
+
+function App() {
+
+  return (
+    <div
+      className='bg-red-500'
+    >
+      <Button>
+        Hello
+      </Button>
+    </div>
+  )
+}
+
+export default App
